f1c93a73cb9611d63e35a79f76fc816e6ec217f4
Shontai Blye <blye.career@gmail.com>
2026-09-14 12:23:50 -0400

    Wave 3 function complete

--- adagrams/game.py
+++ adagrams/game.py
@@ -75,7 +75,53 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    #based on letter points value given in read me
+    #dictionary to store point value for each letter
+    letter_points = {
+        "A": 1,
+        "B": 3,
+        "C": 3,
+        "D": 2,
+        "E": 1,
+        "F": 4,
+        "G": 2,
+        "H": 4,
+        "I": 1,
+        "J": 8,
+        "K": 5,
+        "L": 1,
+        "M": 3,
+        "N": 1,
+        "O": 1,
+        "P": 3,
+        "Q": 10,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "U": 1,
+        "V": 4,
+        "W": 4,
+        "X": 8,
+        "Y": 4,
+        "Z": 10
+    }
+
+    #starting score
+    score = 0
+
+    #convert to uppercase
+    word = word.upper()
+
+    #loop through each letter
+    for letter in word:
+        #get point value and add to score
+        score += letter_points[letter]
+
+    #if word has >= 7 letters add 8 point bonus
+    if len(word) >= 7:
+        score += 8
+
+    return score
 
 def get_highest_word_score(word_list):
     pass
